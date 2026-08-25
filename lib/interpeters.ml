df4e1573bdc1a9da8b95c4169da12dcefaa375e7
TaleWind28 <106275354+TaleWind28@users.noreply.github.com>
2026-08-25 16:26:33 +0200

    Commentate tutte le eval ed i loro pattern matching

--- lib/interpeters.ml
+++ lib/interpeters.ml
@@ -66,99 +66,102 @@ module AbsInterp (D : DOMAIN) = struct
     | Or (cd1, cd2) -> And(negate_cond cd1, negate_cond cd2)
     | Comparison (e1,comp,e2) -> Comparison(e1,negate_comp comp ,e2)
   
-
+    (* Valutazione Espressioni *)
     let rec eval_exp (exp : exp) (st : state) : D.t =
+    (* Controllo lo stato *)
     match st with
-    | BottomEnv -> D.bottom
+    | BottomEnv -> D.bottom (* Se sono in bottomEnv si è verificato un'errore => restituisco il valore di bottom del dominio in analisi *)
     | Env st -> 
         match exp with
-        | Const n ->
-            D.abstract_int n
-        | Var s ->
+        | Const n -> (* Astraggo il valore *)
+            D.abstract_int n 
+        | Var s -> (* Cerco nello stato il valore associato alla variabile s *)
             (
             match Hashtbl.find_opt st s with
                 | Some v -> v
                 | None   -> D.top
             )
-        | BinaryOperation (e1, Add, e2) ->
+        | BinaryOperation (e1, Add, e2) -> (* valuto la somma *)
             D.sum (eval_exp e1 (Env(st))) (eval_exp e2 (Env(st)))
-        | BinaryOperation (e1, Sub, e2) ->
+        | BinaryOperation (e1, Sub, e2) -> (* valuto la sottrazione cambiando di segno il secondo membro per avere così una somma algebrica *)
             D.sum (eval_exp e1 (Env(st))) (D.negate(eval_exp e2 (Env(st))))
-        | BinaryOperation (e1, Mul, e2) ->
+        | BinaryOperation (e1, Mul, e2) -> (* valuto la moltiplicazione *)
             D.mul (eval_exp e1 (Env(st))) (eval_exp e2 (Env(st)))
-        | BinaryOperation (e1, Div, e2) ->
+        | BinaryOperation (e1, Div, e2) -> (* valuto la divisione *)
             D.div (eval_exp e1 (Env(st))) (eval_exp e2 (Env(st)))
-        | UnaryOperation (Negation, e1) ->
+        | UnaryOperation (Negation, e1) -> (* valuto la negazione, ossia il cambio di segno *)
             D.negate (eval_exp e1 (Env(st)))
-        | Random (a, b) ->
+        | Random (a, b) -> (* valuto un valore intero non deterministico *)
             D.abstract_range a b
-
+    
+    (* Valutazione Condizioni *)
     let rec eval_cond (cond : cond) (env : state) :  state = 
         match env with 
-        |BottomEnv -> BottomEnv
+        |BottomEnv -> BottomEnv (* Se sono in bottomEnv si è verificato un'errore => restituisco BottomEnv *)
         | Env env -> 
             match cond with
-            | Boolean true -> Env(env)
-            | Boolean false -> BottomEnv
-            | Not cd -> 
+            | Boolean true -> Env(env) (* Mantengo lo stato invariato *)
+            | Boolean false -> BottomEnv (* Segnalo un'errore *)
+            | Not cd -> (* Nego la condizione *)
                 eval_cond (negate_cond cd) (Env( env ))
-            | And (cd1,cd2) -> 
+            | And (cd1,cd2) -> (* And Logico tra le condizioni *)
                 let env' = eval_cond cd1 (Env(env)) in 
                 eval_cond cd2 env'
-            | Or (cd1,cd2) -> 
+            | Or (cd1,cd2) -> (* Or Logico tra le condizioni *)
                 let env1 = eval_cond cd1 (Env(Hashtbl.copy env)) in
                 let env2 = eval_cond cd2 (Env(Hashtbl.copy env)) in
                 lub_env env1 env2
-            | Comparison (e1,comp,e2) -> 
-                let val1 = eval_exp e1 (Env(env)) in 
-                let val2 = eval_exp e2 (Env(env)) in 
-                match comp with
+            | Comparison (e1,comp,e2) -> (* Comparazione tra espressioni mediante un comparatore*) 
+                let val1 = eval_exp e1 (Env(env)) in (* Valuto e1 *)
+                let val2 = eval_exp e2 (Env(env)) in (* Valuto e2 *)
+                match comp with (* Pattern Matching per applicare il comparatore richiesto *)
+                (* compare_type viene implementato dal dominio in analisi restituisce:
+                    1 con val1 > val2
+                    -1 in caso di val1 < val2 
+                    0 in caso di uguaglianza 
+                    2 in caso di indecidibilità  (es: nei segni pos > pos)
+                *)
                 | Equals -> eval_cond (Boolean(let condition = (D.compare_type val1 val2) in condition == 0 || condition == 2)) (Env(env))
                 | NotEquals -> eval_cond (Boolean(let condition = (D.compare_type val1 val2) in condition != 0)) (Env(env))
                 | Bigger -> eval_cond (Boolean(let condition = (D.compare_type val1 val2) in condition == 1 || condition == 2)) (Env(env))
                 | Smaller -> eval_cond (Boolean(let condition = (D.compare_type val1 val2) in condition == -1 || condition == 2)) (Env(env))
                 | BiggerEquals -> eval_cond(Or(Comparison(e1,Bigger,e2),Comparison(e1,Equals,e2))) (Env(env))
                 | SmallerEquals -> eval_cond(Or(Comparison(e1,Smaller,e2),Comparison(e1,Equals,e2))) (Env(env))
-                
+
+    (* Valutazione Comandi *)
     let rec eval_cmd (command : cmd) (env : state) : state =
          match env with 
-        |BottomEnv -> BottomEnv
+        |BottomEnv -> BottomEnv (* Se sono in bottomEnv si è verificato un'errore => restituisco BottomEnv *)
         | Env env -> 
             match command with
-            | Assign(ide,exp) -> 
-                let v =  eval_exp exp (Env(env)) in 
-                Hashtbl.replace env ide v;
-                Env(env)
-            | Sequence(c1,c2) -> 
-                let env1  = eval_cmd c1 (Env(env)) in 
-                eval_cmd c2 env1
+            | Assign(ide,exp) -> (* Assegnamento di un valore ad un identificatore *)
+                let v =  eval_exp exp (Env(env)) in (* Valuto l'espressione*)
+                Hashtbl.replace env ide v; (* Rimpiazzo il valore se presente, altrimenti viene creata una nuova entry*)
+                Env(env) (* Restituisco lo stato aggiornato *)
+            | Sequence(c1,c2) -> (* Sequenza di Comandi *)
+                let env1  = eval_cmd c1 (Env(env)) in  (* Valuto il primo memorizzando l'ambiente risultante *)
+                eval_cmd c2 env1 (* Valuto il secondo utilizzando l'ambiente risultante dalla valutazione del primo *)
 
-            | Filter(cd) -> eval_cond cd (Env(env)) 
+            | Filter(cd) -> eval_cond cd (Env(env)) (* Controllo se una condizione è rispettata *)
 
-            | Skip -> Env(env)
+            | Skip -> Env(env) (* Skip *)
 
-            | If(cond,thencmd,elsecmd) -> 
+            | If(cond,thencmd,elsecmd) -> (* Istruzione Condizionale i cui rami then ed else vengono sempre valutati e successivamente tramite lub si restringe lo stato *)
                 let e1 = eval_cmd (Sequence(Filter(cond),thencmd)) (Env(Hashtbl.copy env)) in 
                 let e2 = eval_cmd (Sequence(Filter(Not(cond)),elsecmd)) (Env(Hashtbl.copy env)) in 
                 lub_env e1 e2
                     
-            | While(cond,cmd) -> 
-                let e1 = eval_cmd (Sequence(Filter(cond),cmd)) (Env(env)) in 
-                (match e1 with
-                | Env(env') -> eval_cmd (While(cond,cmd)) e1
-                | BottomEnv -> Env(env)
-                )
-            
-            |WhileInf(cond,cmd) -> 
-                let f x = lub_env (Env(env)) (eval_cmd cmd (eval_cond cond x)) in
-                let lfp f = 
+            | While(cond,cmd) -> (* Ciclo che tramite Least Fixpoint valuta  *)
+                let f x = lub_env (Env(env)) (eval_cmd cmd (eval_cond cond x)) in (* Funzione che si occupa di valutare lo stato aggiornandolo ad ogni iterazione *)
+                let lfp f = (* Tramite funzione ausiliaria iterate lfp restituisce, se possibile, il punto dopo il quale il ciclo smette di produrre risultati che espandono lo stato corrente *)
                     let rec iterate x = 
-                        let x' = widen_env x (f x) in 
-                            if leq_env x' x then x
-                            else iterate x'
-                        in iterate BottomEnv
-                in eval_cond (Not(cond)) (lfp f)
+                        let x' = widen_env x (f x) in (* Viene effettuato un Widening sullo stato attuale e lo stato dopo aver applicato f *) 
+                            if leq_env x' x then x (* Se gli stati sono uguali allora ho raggiunto il Least Fixpoint, altrimenti continuo ad iterare *)
+                            else iterate x' 
+                    in iterate BottomEnv (* Parto dallo stato Vuoto e vado a "salire" *)
+                in eval_cond (Not(cond)) (lfp f) (* Valuto la condizione che fa uscire dal while con lo stato una volta raggiunto il Least Fixpoint *)
 
+    (* Funzione eval generale *)
     let eval (prog : cmd) : state =
         let initial_env = Env(Hashtbl.create 10) in
         eval_cmd prog initial_env
